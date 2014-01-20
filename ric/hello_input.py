099554d5afcb76bbc3c655f7edb479422a30eb45
John Montgomery <johnbmontgomery@gmail.com>
2014-01-20 06:15:38 -0500

    Restructured ric's files and added some of lesson plan 2

--- ric/hello_input.py
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/env python3
-
-name = input("What's your name?")
-print("Hello " + name)
--- ric/hello_variable.py
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env python3
-
-name = "John"
-print("Hello " + name)
-number1=2
-number2=4
-print(str(number1) + " plus " + str(number2) + " equals " + str(number1+number2))
